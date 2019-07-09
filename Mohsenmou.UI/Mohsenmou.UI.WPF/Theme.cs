b9725bb0d1479c2c84ba278d51672ae403b134dc
mohsen mousavi <mohsenmou@gmail.com>
2019-07-10 02:22:46 +0430

    add Theme class to project

---
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/Theme.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Mohsenmou.UI.WPF
+{
+    public sealed class Theme
+    {
+        [ThreadStatic]
+        private static ResourceDictionary resourceDictionary;
+
+        internal static ResourceDictionary ResourceDictionary
+        {
+            get
+            {
+                if (resourceDictionary!=null)
+                {
+                    return resourceDictionary;
+                }
+                resourceDictionary = new ResourceDictionary();
+                LoadThemeType(ThemeType.Light);
+                return resourceDictionary;
+            }
+        }
+
+        public static ThemeType ThemeType { get; set; } = ThemeType.Light;
+
+        private static void LoadThemeType(ThemeType type)
+        {
+            ThemeType = type;
+            switch (type)
+            {
+                case ThemeType.Light:
+                    break;
+                case ThemeType.Dark:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static object GetResource(ThemeResourceKey resourceKey)
+        {
+            return ResourceDictionary.Contains(resourceKey.ToString()) ? 
+                ResourceDictionary[resourceKey.ToString()] : null;
+        }
+
+        internal static void SetResource(object key,object resource)
+        {
+            ResourceDictionary[key] = resource;
+        }
+
+        internal static Color ColorFromHex(string colorHex)
+        {
+            return (Color?)ColorConverter.ConvertFromString(colorHex) ?? Colors.Transparent;
+        }
+    }
+}
