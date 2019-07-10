f11d633517caad9899932e5cd1c757f38ad2ac8a
mohsen mousavi <mohsenmou@gmail.com>
2019-07-10 23:23:48 +0430

    add cleartextbutton to textbox control

--- Mohsenmou.UI/Mohsenmou.UI.WPF/AttachedProperties/TextBoxHelper.cs
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/AttachedProperties/TextBoxHelper.cs
@@ -2,13 +2,26 @@
 namespace Mohsenmou.UI.WPF.AttachedProperties
 {
     public static class TextBoxHelper
-    {      
+    {
+        public static readonly DependencyProperty ClearTextButtonProperty =
+            DependencyProperty.RegisterAttached("ClearTextButton", typeof(bool), typeof(TextBoxHelper), new PropertyMetadata(false));
+
         public static readonly DependencyProperty WatermarkProperty =
-            DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(null));
+                    DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(null));
+
+        public static bool GetClearTextButton(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ClearTextButtonProperty);
+        }
 
         public static string GetWatermark(DependencyObject obj)
         {
             return (string)obj.GetValue(WatermarkProperty);
+        }
+
+        public static void SetClearTextButton(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ClearTextButtonProperty, value);
         }
 
         public static void SetWatermark(DependencyObject obj, string value)
---
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/Themes/Styles/ButtonStyles.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Mohsenmou.UI.WPF
+{
+    public partial class ButtonStyles:ResourceDictionary
+    {
+        public ButtonStyles()
+        {
+            InitializeComponent();
+        }
+        private void OnClearTextClick(object sender, RoutedEventArgs e)
+        {
+            TextBox textbox = (TextBox)((FrameworkElement)sender).TemplatedParent;
+            if (textbox!=null)
+            {
+                if (!string.IsNullOrEmpty(textbox.Text))
+                {
+                    textbox.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
