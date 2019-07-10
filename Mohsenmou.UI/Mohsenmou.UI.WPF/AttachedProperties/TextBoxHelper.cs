a1154c96a3dd82316c63f062795ffa58c1cd3b51
mohsen mousavi <mohsenmou@gmail.com>
2019-07-10 18:13:53 +0430

    add watermark to textbox control

---
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/AttachedProperties/TextBoxHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+namespace Mohsenmou.UI.WPF.AttachedProperties
+{
+    public static class TextBoxHelper
+    {      
+        public static readonly DependencyProperty WatermarkProperty =
+            DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(null));
+
+        public static string GetWatermark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+    }
+}
