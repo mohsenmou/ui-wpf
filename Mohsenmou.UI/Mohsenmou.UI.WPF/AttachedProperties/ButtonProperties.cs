f39c7f749d172b32cfc96aa8ab700fe1e42883b9
mohsen mousavi <mohsenmou@gmail.com>
2019-07-10 16:14:23 +0430

    add attached properties - corner radius for button control

---
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/AttachedProperties/ButtonProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Mohsenmou.UI.WPF.AttachedProperties
+{
+    public static class ButtonProperties
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius",
+                typeof(double),
+                typeof(ButtonProperties),
+                new FrameworkPropertyMetadata(0d));
+        public static double GetCornerRadius(DependencyObject obj)
+        {
+            return (double)obj.GetValue(CornerRadiusProperty);
+        }
+        public static void SetCornerRadius(DependencyObject obj, double value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+    }
+}
