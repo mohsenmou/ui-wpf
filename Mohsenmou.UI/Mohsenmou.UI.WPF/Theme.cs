d1158d855b9fe0f1945d3d1aa396ddf5040c5d49
mohsen mousavi <mohsenmou@gmail.com>
2019-07-10 12:43:18 +0430

    define all resources in ThemeResourceKey and Theme class

--- Mohsenmou.UI/Mohsenmou.UI.WPF/Theme.cs
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/Theme.cs
@@ -35,22 +35,69 @@ namespace Mohsenmou.UI.WPF
         {
             ResourceDictionary[key] = resource;
         }
+        private static LinearGradientBrush GetWindowHeaderGradient()
+        {
+            return new LinearGradientBrush
+            {
+                StartPoint = new Point(0, 0.5),
+                EndPoint = new Point(1, 0.5),
+                GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop { Offset = 0, Color = ColorFromHex("#FF833AB4") },
+                        new GradientStop { Offset = 1, Color = ColorFromHex("#FFFD1D1D") }
+                    }
+            };
+        }
         public static void LoadThemeType(ThemeType type)
         {
             ThemeType = type;
+
+            SetResource(ThemeResourceKey.ControlDisabledOpacity.ToString(), 0.4d);
+            SetResource(ThemeResourceKey.ControlDefaultBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFC12B68")));
+            SetResource(ThemeResourceKey.ControlMouseOverBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFA53289")));
+            SetResource(ThemeResourceKey.ControlFocusBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF833AB4")));
+            SetResource(ThemeResourceKey.ListMouseOverBackground.ToString(), new SolidColorBrush(ColorFromHex("#22833AB4")));
+            SetResource(ThemeResourceKey.ListMouseOverBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFA53289")));
+            SetResource(ThemeResourceKey.ListSelectedBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF833AB4")));
+            SetResource(ThemeResourceKey.ListSelectedBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFA53289")));
+            SetResource(ThemeResourceKey.ListSelectedForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
+            SetResource(ThemeResourceKey.WindowBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF616D7B")));
+            SetResource(ThemeResourceKey.WindowActiveBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF833AB4")));
+            SetResource(ThemeResourceKey.WindowControlMouseOverBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF000000")));
+            SetResource(ThemeResourceKey.WindowHeaderBackground.ToString(), GetWindowHeaderGradient());
+            SetResource(ThemeResourceKey.WindowHeaderForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
+
             switch (type)
             {
                 case ThemeType.Light:
-                    SetResource(ThemeResourceKey.ContentBackground.ToString(),
-                        new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
-                    SetResource(ThemeResourceKey.ContentForeground.ToString(),
-                        new SolidColorBrush(ColorFromHex("#FF000000")));
+                    SetResource(ThemeResourceKey.ContentBackground.ToString(), new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
+                    SetResource(ThemeResourceKey.ContentForeground.ToString(), new SolidColorBrush(ColorFromHex("#FF3F3F3F")));
+                    SetResource(ThemeResourceKey.ControlForeground.ToString(), new SolidColorBrush(ColorFromHex("#FF3F3F3F")));
+                    SetResource(ThemeResourceKey.ControlBackground.ToString(), new SolidColorBrush(ColorFromHex("#FFDBE0E4")));
+                    SetResource(ThemeResourceKey.ControlBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF8192A1")));
+                    SetResource(ThemeResourceKey.ControlContentBackground.ToString(), new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
+                    SetResource(ThemeResourceKey.ControlHighlightBackground.ToString(), new SolidColorBrush(ColorFromHex("#77833AB4")));
+                    SetResource(ThemeResourceKey.ControlMouseOverBackground.ToString(), new SolidColorBrush(ColorFromHex("#FFEDEFF2")));
+                    SetResource(ThemeResourceKey.ControlPressedBackground.ToString(), new SolidColorBrush(ColorFromHex("#FFBFC2C4")));
+                    SetResource(ThemeResourceKey.ControlPressedBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF3CC0FF")));
+                    SetResource(ThemeResourceKey.GlyphForeground.ToString(), new SolidColorBrush(ColorFromHex("#FF646769")));
+                    SetResource(ThemeResourceKey.GroupBoxHeaderBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFA53289")));
+                    SetResource(ThemeResourceKey.GroupBoxHeaderForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFA53289")));
                     break;
                 case ThemeType.Dark:
-                    SetResource(ThemeResourceKey.ContentBackground.ToString(),
-                        new SolidColorBrush(ColorFromHex("#FF000000")));
-                    SetResource(ThemeResourceKey.ContentForeground.ToString(),
-                        new SolidColorBrush(ColorFromHex("#FFFFFFFF")));
+                    SetResource(ThemeResourceKey.ContentBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF222529")));
+                    SetResource(ThemeResourceKey.ContentForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFF4F6F9")));
+                    SetResource(ThemeResourceKey.ControlForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFF4F6F9")));
+                    SetResource(ThemeResourceKey.ControlBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF2D3136")));
+                    SetResource(ThemeResourceKey.ControlBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF616D7B")));
+                    SetResource(ThemeResourceKey.ControlContentBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF1C1E22")));
+                    SetResource(ThemeResourceKey.ControlHighlightBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF833AB4")));
+                    SetResource(ThemeResourceKey.ControlMouseOverBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF4C525E")));
+                    SetResource(ThemeResourceKey.ControlPressedBackground.ToString(), new SolidColorBrush(ColorFromHex("#FF222529")));
+                    SetResource(ThemeResourceKey.ControlPressedBorder.ToString(), new SolidColorBrush(ColorFromHex("#FF00ACE0")));
+                    SetResource(ThemeResourceKey.GlyphForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFEBECED")));
+                    SetResource(ThemeResourceKey.GroupBoxHeaderBorder.ToString(), new SolidColorBrush(ColorFromHex("#FFBF399E")));
+                    SetResource(ThemeResourceKey.GroupBoxHeaderForeground.ToString(), new SolidColorBrush(ColorFromHex("#FFBF399E")));
                     break;
             }
         }
--- Mohsenmou.UI/Mohsenmou.UI.WPF/ThemeResourceKey.cs
+++ Mohsenmou.UI/Mohsenmou.UI.WPF/ThemeResourceKey.cs
@@ -3,6 +3,31 @@
     public enum ThemeResourceKey
     {
         ContentBackground,
-        ContentForeground
+        ContentForeground,
+        ControlForeground,
+        ControlBorder,
+        ControlBackground,
+        ControlContentBackground,
+        ControlDefaultBorder,
+        ControlHighlightBackground,
+        ControlMouseOverBackground,
+        ControlMouseOverBorder,
+        ControlPressedBackground,
+        ControlPressedBorder,
+        ControlDisabledOpacity,
+        ControlFocusBorder,
+        GlyphForeground,
+        GroupBoxHeaderBorder,
+        GroupBoxHeaderForeground,
+        ListMouseOverBackground,
+        ListMouseOverBorder,
+        ListSelectedBackground,
+        ListSelectedBorder,
+        ListSelectedForeground,
+        WindowBorder,
+        WindowActiveBorder,
+        WindowControlMouseOverBackground,
+        WindowHeaderBackground,
+        WindowHeaderForeground
     }
 }
